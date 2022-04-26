fed315950a46f7820a25ae4312f7110755375902
TimeGlitch <frankj.zhang@gmail.com>
2022-04-26 10:20:29 -0700

    update

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ APIKEY = config.SECRET_KEY
 import os
 import openai
 
+openai.api_key= APIKEY
+openai.organization = "org-qNYVl0J71153owejeurZzzCm"
+
+listofengines = openai.Engine.list()
+
+openai.Completion.create(
+  engine="text-davinci-002",
+  prompt="Say this is a test",
+  max_tokens=5
+)
+
 
 
 
